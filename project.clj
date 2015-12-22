865e67d0f068e8d1012a70502e401ec1a28cdc22
jonneale <jonneale@gmail.com>
2015-12-22 16:08:31 +0000

    Added some deps

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [digest "1.4.4"]])
