b984cce65df0d3843296baf866fa16c7bfb2614c
Jon Neale <jon.neale@USW-00589.local>
2016-12-02 12:08:57 +0000

    Better solution for 2016/1

--- src/advent_of_code/2016/1.clj
+++ src/advent_of_code/2016/1.clj
@@ -4,75 +4,69 @@
   (map #(clojure.string/replace % #"[^A-Z0-9]" "") (clojure.string/split (slurp "resources/2016-1.txt") #",")))
 
 (def state-machine
-  {["N" "R"] "E"
-   ["N" "L"] "W"
-   ["E" "R"] "S"
-   ["E" "L"] "N"
-   ["S" "R"] "W"
-   ["S" "L"] "E"
-   ["W" "R"] "N"
-   ["W" "L"] "S"})
+  {[\N \R] \E
+   [\N \L] \W
+   [\E \R] \S
+   [\E \L] \N
+   [\S \R] \W
+   [\S \L] \E
+   [\W \R] \N
+   [\W \L] \S})
 
 (defmulti move
-  (fn [direction distance x y]
-    (str direction)))
+  (fn [direction]
+    direction))
 
-(defmethod move "N"
-  [direction distance x y]
-  [x (+ y distance)])
+(defmethod move \N
+  [_]
+  [0 1])
 
-(defmethod move "S"
-  [direction distance x y]
-  [x (- y distance)])
+(defmethod move \S
+  [_]
+  [0 -1])
 
-(defmethod move "W"
-  [direction distance x y]
-  [(- x distance) y])
+(defmethod move \W
+  [_]
+  [-1 0])
 
-(defmethod move "E"
-  [direction distance x y]
-  [(+ x distance) y])
+(defmethod move \E
+  [_]
+  [1 0])
 
-(defn follow-input
-  [[starting-direction x y] input]
-  (let [[direction & distance] input
-        movement-direction (state-machine [(str starting-direction) (str direction)])
-        [result-x result-y] (move movement-direction (read-string (apply str distance)) x y)]
-    [movement-direction result-x result-y]))
+(defn map-input-to-transition
+  [[current-direction transitions] [direction & distance]]
+  (let [new-direction (state-machine [current-direction direction])
+        int-distance  (read-string (apply str distance))]
+    [new-direction (into transitions (repeat int-distance (move new-direction)))]))
+
+(defn generate-route
+  [inputs]
+  (reduce map-input-to-transition [\N []] inputs))
 
 (defn follow-route
-  [input]
-  (reductions follow-input ["N" 0 0] input))
+  [[old-x old-y] [d-x d-y]]
+  [(+ old-x d-x) (+ old-y d-y)])
 
-(defn abs
-  [x]
-  (if (zero? x) x
-      (Math/sqrt (* x x))))
+(defn all-interim-positions
+  [inputs]
+  (reductions follow-route [0 0] (last (generate-route inputs))))
 
-(defn find-smallest-second
-  [values]
-  (reduce (fn [best-so-far points]
-            (let [latest-visit (reduce max (map first points))]
-              (if (> (first best-so-far) latest-visit)
-                (first points)
-                best-so-far))) 
-          [999999]
-          (map last values)))
+(defn distance-to-position
+  [position]
+  (reduce + (map #(Math/abs %) position)))
 
-(defn first-visited-twice
-  [input]
-  (find-smallest-second
-   (filter #(> (count (second %)) 1)
-           (group-by (juxt last (comp last butlast)) 
-                     (map-indexed (fn [ix v] (cons ix v)) (follow-route input))))))
+(defn distance-to-final-position
+  [inputs]
+  (distance-to-position (last (find-final-position inputs))))
 
-(defn distance-to-point-visited-twice
-  [input]
-  (reduce +
-          (map abs
-               (drop 2
-                     (find-smallest-second input)))))
-
-(defn run-to-end
-  [input]
-  (reduce + (map abs (rest (last (follow-route input))))))
+(defn first-position-visited-twice
+  [inputs]
+  (let [numbered-positions (map-indexed #(cons %1 %2) (all-interim-positions inputs))]
+    (->> numbered-positions 
+         (group-by (fn [[_ x y]] [x y]))
+         (filter #(> (count (last %)) 1))
+         (map (fn [[position occurences]] [position (reduce max (map first occurences))]))
+         (sort-by last)
+         first
+         first
+         distance-to-position)))
