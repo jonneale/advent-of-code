ab028e1ac9ebf7e6c259de5b24e4d2b9863f50c7
Jon Clay <jonneale@gmail.com>
2022-12-05 14:19:19 +0000

    Updated shell script to be more valuable and easier to run

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+TODAY=`date +'%-d'`
+THIS_YEAR=`date +'%Y'`
 
-YEAR=$1
-DAY=$2
+YEAR=${1:-$THIS_YEAR}
+DAY=${2:-$TODAY}
 
 `curl https://adventofcode.com/${YEAR}/day/${DAY}/input --cookie "session=$SESSION" > ./resources/${YEAR}/${DAY}.txt`
 
-`touch ./src/advent_of_code/${YEAR}/${DAY}.clj`
+`echo "(ns advent-of-code.${YEAR}.${DAY}
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+(def input
+    (->> (-> (io/resource \"${YEAR}/${DAY}.txt\")
+              slurp
+             (clojure.string/split  #\"\\n\"))
+       (map #(clojure.string/split % #\"\\,\"))))" > ./src/advent_of_code/${YEAR}/${DAY}.clj`
+
